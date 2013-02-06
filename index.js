7e9ccf56003ac4b163c9d3f4de8598ce70ca5da9
Isao Yagi <isao@yahoo-inc.com>
2013-02-06 12:37:24 -0800

    wip, works

--- index.js
+++ index.js
@@ -1,23 +1,55 @@
-
+/**
+ * Copyright (c) 2013 Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the MIT License.
+ * See the accompanying LICENSE file for terms.
+ */
+/*jshint node:true*/
+'use strict';
 
 function Route(routes) {
-    var name_re = /(?=:)\w+/g;
-
-	routes.forEach(function(route) {
-		var names = route.pattern.match(name_re);
-
-		console.log(names);
-	});
+    //this.routes = this.compile(routes);
+    this.compile(routes);
 }
 
-Route.prototype.get = 
-Route.prototype.for = function (str) {
-    
-    return {
-        action: 1,
-        param: 1,
-        parts: 1
+Route.prototype.compile = function(routes) {
+    var self = this,
+        SYMBOLS = /([\/.+\^$(){}\[\]])/g,
+        NAME_RE = /:(\w+)/g;
+
+    self.routes = [];
+    if(!self.param) {
+        self.param = [];
     }
+
+    function perRoute(route) {
+        route.parts = [];
+
+        function replaceCb(ignored, name) {
+            route.parts.push(name);
+            return '(\\w+)';
+        }
+
+        route.regex = new RegExp(
+            route.pattern.replace(SYMBOLS, '\\$1').replace(NAME_RE, replaceCb));
+
+        self.routes.push(route);
+    }
+    routes.forEach(perRoute);
+}
+
+Route.prototype.of =
+Route.prototype.given = function (str) {
+    var found = false;
+
+    function checkRoute(route) {
+        if(route.regex.test(str)) {
+            found = route;
+            return true;
+        }
+    }
+
+    this.routes.some(checkRoute);
+    return found;
 };
 
 module.exports = Route;
--- test.js
+++ test.js
@@ -1,50 +1,9 @@
-function Route(routes) {
-    var self = this,
-        SYMBOLS = /([\/.+\^$(){}\[\]])/g,
-        NAME_RE = /:(\w+)/g;
-
-    self.routes = [];
-    if(!self.param) {
-        self.param = [];
-    }
-
-    function perRoute(route) {
-        route.parts = [];
-
-        function replaceCb(ignored, name) {
-            route.parts.push(name);
-            return '(\\w+)';
-        }
-
-        route.regex = new RegExp(
-            route.pattern.replace(SYMBOLS, '\\$1').replace(NAME_RE, replaceCb));
-
-        self.routes.push(route);
-    }
-
-    routes.forEach(perRoute);
-}
-
-Route.prototype.given = function (str) {
-    var i = 0,
-        found = false;
-
-    for(; i < this.routes.length; i++) {
-        if(this.routes[i].regex.test(str)) {
-            found = this.routes[i];
-            break;
-        }
-    }
-
-    return found;
-};
-
-
-var test = require('tape');
+var test = require('tape'),
+    Route = require('./');
 
 test('1', function(t) {
-
-    var config = [
+    var route,
+        config = [
             {
                 "pattern": "/modules/:modname/:filename.json",
                 "action": "modconfig"
@@ -53,15 +12,17 @@ test('1', function(t) {
                 "pattern": "/mojits/:mojitname/:controller.:affinity.js",
                 "action": "controller"
             }
-        ],
-        route = new Route(config);
+        ];
 
-    t.ok(route.given('abc/modules/mymod/config.json'));
-    t.ok(route.given('/mojits/foo/foo.server.js'));
-    t.notOk(route.given('/abc/def/ghi'));
+    route = new Route(config);
 
-//     t.true('action' in match);
-//     t.true('parts' in match);
-//     t.true('param' in match);
+    t.ok(route.of('abc/modules/mymod/config.json'));
+    t.ok(route.of('/mojits/foo/foo.server.js'));
+    t.ok(route.of('/mojits/_/ghi.jkl.js'));
+
+    t.notOk(route.of('modules/mymod/config.json'));
+    t.notOk(route.of('/mojits//ghi.jkl'));
+    t.notOk(route.of('/abc/def/ghi'));
     t.end();
 });
+
