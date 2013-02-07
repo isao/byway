77f525a53f2ae7ba71e7dcd5543bfdf70e0dc620
Isao Yagi <isao@yahoo-inc.com>
2013-02-06 17:42:35 -0800

    update tests

--- tests/simple.js
+++ tests/simple.js
@@ -1,24 +1,19 @@
 var test = require('tape'),
     Byway = require('../');
 
-test('matched :routes', function(t) {
-    var byway,
-        config = [
-            {"pattern": "/modules/:modname/:filename.json"},
-            {"pattern": "/mojits/:mojitname/:controller.:affinity.js"}
-        ];
+test('match :routes', function(t) {
+    var config = [{"pattern": "/mojits/:mojitname/:controller.:affinity.js"}],
+        byway = new Byway(config);
 
-    byway = new Byway(config);
-
-    t.ok(byway.of('abc/modules/mymod/config.json'));
+    t.ok(byway.of('abc/mojits/foo/foo.server.js'));
+    t.ok(byway.of('/abc/def/mojits/foo/foo.server.js'));
     t.ok(byway.of('/mojits/foo/foo.server.js'));
     t.ok(byway.of('/mojits/_/ghi.jkl.js'));
     t.end();
 });
 
 test('unmatched :routes', function(t) {
-    var byway,
-        config = [
+    var config = [
             {"pattern": "/modules/:modname/:filename.json"},
             {"pattern": "/mojits/:mojitname/:controller.:affinity.js"}
         ];
@@ -30,16 +25,59 @@ test('unmatched :routes', function(t) {
     t.notOk(byway.of('/abc/def/ghi'));
     t.end();
 });
-// 
-// test('ok regexes', function(t) {
-//     var byway,
-//         config = [
-//             {"pattern": "/\/modules\/(\w+)/(\w+).json", "isregex": true},
-//             {"pattern": "\/mojits/(\w+)/(\w+).(\w+).js", "isregex": 1}
-//         ];
-// 
-//     byway = new Byway(config);
-// 
-//     t.ok(byway.of('abc/modules/mymod/config.json'));
-//     t.end();    
-// });
+
+test('match /modules/:modname/:filename.json', function(t) {
+    var config = [{"pattern": "/modules/:modname/:filename.json"}]
+        byway = new Byway(config);
+
+    t.ok(byway.of('abc/modules/mymod/config.json ohhai!'), 'match not $anchored');
+    t.ok(byway.of('/modules/mymod/config.json'), 'match not ^anchored');
+    t.ok(byway.of('/abc/def/modules/mymod/config.json'), 'match not ^anchored');
+    t.ok(byway.of('/modules/MYMODULE/FOOBAR.json'), 'named params match case-insensitively');
+    t.ok(byway.of('abc/moDUles/mymod/config.JSON'), 'all letters match case-insensitively');
+    t.end();
+});
+
+test('match anchored$ /modules/:modname/:filename.json$', function(t) {
+    var config = [{"pattern": "/modules/:modname/:filename.json$"}]
+        byway = new Byway(config);
+
+    t.ok(byway.of('/modules/mymod/config.json'), 'match not ^anchored');
+    t.ok(byway.of('/abc/def/modules/mymod/config.json'), 'match not ^anchored');
+    t.ok(byway.of('/modules/MYMODULE/FOOBAR.json'), 'named params match case-insensitively');
+    t.ok(byway.of('abc/moDUles/mymod/config.JSON'), 'all letters match case-insensitively');
+    t.notOk(byway.of('abc/modules/mymod/config.json ohhai!'), 'match is $anchored');
+    t.end();
+});
+
+test('match ^anchored ^/modules/:modname/:filename.json', function(t) {
+    var config = [{"pattern": "^/modules/:modname/:filename.json"}]
+        byway = new Byway(config);
+
+    t.ok(byway.of('/modules/mymod/config.json ohhai!'), '^ but not $');
+    t.ok(byway.of('/modules/mymod/config.json'), '^');
+    t.ok(byway.of('/modules/MYMODULE/FOOBAR.json'), 'named params match case-insensitively');
+    t.notOk(byway.of('/abc/def/modules/mymod/config.json'), 'match not ^anchored');
+    t.notOk(byway.of('abc/moDUles/mymod/config.JSON'), 'all letters match case-insensitively');
+    t.end();
+});
+
+test('match ^anchored$ ^/modules/:modname/:filename.json$', function(t) {
+    var config = [{"pattern": "^/modules/:modname/:filename.json"}]
+        byway = new Byway(config);
+
+    t.ok(byway.of('/modules/mymod/config.json'));
+    t.notOk(byway.of('abc/modules/mymod/config.json'));
+    t.notOk(byway.of('/modules/mymod/config.json/cheezits'));
+    t.end();
+});
+
+test('matched regexes -- beware the crazy escaping!', function(t) {
+    var byway,
+        config = [{"pattern": "\\/modules\\/(\\w+)\\/(\\w+).json", "isregex": true}];
+
+    byway = new Byway(config);
+    t.ok(byway.of('abc/modules/mymod/config.json'));
+    t.end();
+});
+
