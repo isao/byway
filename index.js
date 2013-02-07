d60c051c74b1e0fa957a20f2cf6970c5afc553cb
Isao Yagi <isao@yahoo-inc.com>
2013-02-06 21:05:42 -0800

    actual return value for of()

--- index.js
+++ index.js
@@ -7,8 +7,8 @@
 'use strict';
 
 var SYMBOLS = /([\/.+(){}\[\]])/g, //escape /.+(){}[] before :name conversions
-    NAME_IN = /:(\w+)/g,           //regex to extract :names from patterns
-    NAME_RE = '(\\w+)';            //string for :name matching in final regex
+    NAME_IN = /:(\w+)/g,           //for extracting :names from patterns
+    NAME_RE = '(\\w+)';            //for final regex in place of :names
 
 
 function Byway(routes) {
@@ -16,23 +16,49 @@ function Byway(routes) {
 }
 
 Byway.prototype.of = function(str) {
-    return this.routes.some(function checkRoute(route) {
-        return route.regex.exec(str) ? route : false;
-    });
+    var found = false;
+    function checkRoute(route) {
+        found = route.regex.exec(str);
+        if(found) {
+            found = {
+                'input': found.input,
+                'param': route.param || [],
+                'parts': parts(route.names, found.slice(1))
+            };
+            return true;
+        }
+    }
+
+    this.routes.some(checkRoute);
+    return found;
 };
+
+function parts(names, vals) {
+    var out;
+    if(names.length) {
+        out = {};
+        names.forEach(function copy(name, i) {
+            out[name] = vals[i];
+        });
+    } else { //pattern was a regex, so no names
+        out = vals.slice(0);
+    }
+
+    return out;
+}
 
 function compile(routes) {
     function perRoute(route) {
         var pattern;
-        route.names = []; //todo
 
-        function partnames(ignored, name) {
+        route.names = [];
+        function partnamer(ignored, name) {
             route.names.push(name);
             return NAME_RE;
         }
 
         pattern = route.isregex ? route.pattern :
-            route.pattern.replace(SYMBOLS, '\\$1').replace(NAME_IN, partnames);
+            route.pattern.replace(SYMBOLS, '\\$1').replace(NAME_IN, partnamer);
 
         route.regex = new RegExp(pattern, 'i');
     }
