fc5d142979d817fa95d83928551a7fa7d9b4ae15
Isao Yagi <isao@yahoo-inc.com>
2013-07-26 16:28:21 -0700

    noop (note about listening to tap's events)

--- tests/index.js
+++ tests/index.js
@@ -1,7 +1,23 @@
 //require all .js files in __dirname
 
 var fs = require('fs'),
-    resolve = require('path').resolve;
+    resolve = require('path').resolve,
+
+    tap = require('tap'), // global harness
+    out = tap.output;     // test result stream
+
+
+// you can listen to tap events, nice.
+//
+// out.on('error', function(str) {
+//     console.log('ERR>>', str);
+// });
+// out.on('data', function(str) {
+//     console.log('>>', str);
+// });
+// out.on('end', function(err, count, ok) {
+//     console.log('>>', err, count, ok);
+// });
 
 function load(name) {
     var path;
